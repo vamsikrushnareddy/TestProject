76933208620f5c24d11354a97c94ebd88f754fa3
vamsikrushnareddy <36809744+vamsikrushnareddy@users.noreply.github.com>
2018-08-22 11:19:16 +0530

    newly added

--- TestProject/src/com/pol/Test.java
+++ TestProject/src/com/pol/Test.java
@@ -2,7 +2,7 @@ package com.pol;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Hi VamsikrishnaReddy rachepalli...............!");
+		System.out.println("Hi VamsikrishnaReddy rachepalli  ...............!");
 		
 	}
 }
