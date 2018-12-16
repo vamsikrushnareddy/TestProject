59729656301db6053c65e6798e85833eff324bb3
vamsikrushnareddy <36809744+vamsikrushnareddy@users.noreply.github.com>
2018-12-16 10:16:07 +0530

    added new syste.out

--- TestProject/src/com/pol/Test.java
+++ TestProject/src/com/pol/Test.java
@@ -3,6 +3,6 @@ package com.pol;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hi VamsikrishnaReddy rachepalli  ...............!");
-		
+		System.out.println("Ready..........................!");
 	}
 }
